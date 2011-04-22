3428cb143861c257c79ceb117a7460934b6efbf8
Lawrence Chang <lachang@yahoo.com>
2011-04-22 08:57:34 -0700

    init commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,24 @@
+module ApplicationHelper
+
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+    link_to logo, root_path
+  end
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
+  def gravatar_for(user, options = { :size => 50 })
+    gravatar_image_tag(user.email.downcase, 
+          :alt => user.name, 
+          :class => "gravatar", 
+          :gravatar => options)
+  end
+end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Lawrence Chang"
+  user.email                  "lachang@yahoo.com"
+  user.password               "j2ee123"
+  user.password_confirmation  "j2ee123"
+end
